a5894e504d2d1bdfa4a69ce41157327bca8735a9
Стас Басов <stas.basov@whatever.com>
2023-04-23 11:35:13 +0300

    Add docstring

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from random import randint
 
+# Новый импорт.
+# Из модуля start_game_banner, который расположен в папке graphic_arts,
+# импортируем функцию run_screensaver().
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
@@ -79,6 +84,7 @@ def choice_char_class() -> str:
 
 
 def main() -> str:
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
